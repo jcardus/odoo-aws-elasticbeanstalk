d52dbe8f6f7f48ee4f843278801a8c647166786d
jcardus <asklocation.net@gmail.com>
2022-10-16 15:03:36 +0100

    Create __manifest__.py

---
+++ addons/bus/__manifest__.py
@@ -0,0 +1,16 @@
+
+{
+    'name' : 'IM Bus',
+    'version': '1.0',
+    'category': 'Hidden',
+    'complexity': 'easy',
+    'description': "Instant Messaging Bus allow you to send messages to users, in live.",
+    'depends': ['base', 'web'],
+    'data': [
+        'views/bus.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+    'auto_install': True,
+}
